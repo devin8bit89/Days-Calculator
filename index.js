22b35bf7c64740e20d617a07c9e72e404bb7a1da
MuzeLust <131277143+devin8bit89@users.noreply.github.com>
2024-05-31 11:19:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-
 const date = new Date();
 
+
+
 function getUserGoal() {
+
     let userGoal = document.getElementById("userGoal").value;
+
     console.log(userGoal);
+
     document.getElementById("user-defined-goal").innerHTML = userGoal;
+
     document.getElementById("userGoalDiv").style.display = 'none';
+
 }
 
+
+
 function getDays() {
+
     let userDateInput = document.getElementById("userDate").value;
+
     let userDate = new Date(userDateInput);
+
     let timeDiff = userDate.getTime() - date.getTime();
+
     let dayDiff = timeDiff / (1000 * 60 * 60 * 24);
+
     let conv = Math.ceil(dayDiff)
+
     console.log(conv)
+
     let counter = document.getElementById("count").innerHTML = conv; 
+
     document.getElementById("text").style.display = 'block';
     if (conv < 2) {
         document.getElementById("text").innerHTML="day left"
     }
     if (conv == -1) {
         document.getElementById("text").innerHTML="day ago"
+        let pos = conv * -1
+        document.getElementById("count").innerHTML=pos;
     }
     if (conv < -1) {
         document.getElementById("text").innerHTML="days ago"
+        let pos = conv * -1
+        document.getElementById("count").innerHTML=pos;
     }
     if (conv == 0) {
         document.getElementById("text").innerHTML=""
@@ -34,7 +54,11 @@ function getDays() {
         document.getElementById("text").innerHTML="days left"
     }
     
+
 }
+
+
+
 
 
 
